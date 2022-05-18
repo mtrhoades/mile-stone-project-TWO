168b85b57a64181c33f513564d3958e28160d7c4
root <mtrhoades0727@gmail.com>
2022-05-17 16:20:15 -1000

    started react front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+// IMPORTS
+import './App.css';
+import StockList from './components/StockList';
+import AddNewStock from './components/AddNewStock';
+
+
+// FUNCTIONAL COMPONENT
+function App() {
+  // VANILLA JS SECTION
+
+
+
+  // JSX SECTION
+  return (
+    <div className="App">
+
+      <StockList />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/DeleteStock.js
@@ -0,0 +1,30 @@
+// IMPORTS
+import React from 'react';
+
+
+// FUNCTIONAL COMPONENT
+const DeleteStock = () => {
+        // VANILLA JS SECTION
+
+
+
+    
+   // useState section:
+   
+
+   // useEffect section:
+
+
+   // helper function section:
+
+
+
+
+    // JSX SECTION
+  return (
+    <div>Delete Stock</div>
+  )
+}
+
+
+export default DeleteStock;
