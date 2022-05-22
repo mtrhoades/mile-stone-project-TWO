95fc61380c6c2c1eedb97ee7c0a8957994db4f57
root <mtrhoades0727@gmail.com>
2022-05-22 07:48:29 -1000

    update routes complete!

--- frontend/src/components/UpdateStock.js
+++ frontend/src/components/UpdateStock.js
@@ -13,18 +13,26 @@ const UpdateStock = ({ stock }) => {
    const [stock_name, setStockName] = useState(stock.stock_name);
    const [price, setPrice] = useState(stock.price);
 
-
    const [show, setShow] = useState(false); // for modal
+
 
 
    // useEffect section:
 
 
    // helper function section:
+
+// modal helper functions:
+const handleClose = () => setShow(false); // closing the modal
+const handleShow = () => setShow(true); // opening the modal
+
+
+
+// fetch request
 const updateStock = async (e) => {
   e.preventDefault();
   try {
-    const body = { symbol };
+    const body = { symbol, stock_name, price };
     const response = await fetch(`http://localhost:3006/stocks/${stock.stock_id}`, {
       method: 'PUT',
       headers: {"Content-Type": "application/json"},
@@ -40,9 +48,6 @@ const updateStock = async (e) => {
   }
 }
 
-// modal functions:
-const handleShow = () => setShow(true); // opening the modal
-const handleClose = () => setShow(false); // closing the modal
 
 
 
@@ -56,77 +61,78 @@ const handleClose = () => setShow(false); // closing the modal
         class="btn btn-outline-success btn-sm"
         data-toggle="modal"
         data-target={`#id${stock.stock_id}`}
+        onClick={handleShow}
       >
         Edit
       </button>
 
       {/* <!-- Modal --> */}
       <div class="modal fade" id={`id${stock.stock_id}`} tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-              <div class="modal-content">
-                  <div class="modal-header">
-                      <h5 class="modal-title" id="exampleModalLabel">Edit To-Do</h5>
-                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                      </button>
-                  </div>
-                  <div class="modal-body">
-                  <form>
-            <div class="form-group row">
-              <label for="Symbol" class="col-sm-2 col-form-label" >Symbol</label>
-              <input 
-                type="text"
-                class="form-control form-control-sm"
-                id="Symbol"
-                placeholder="Symbol"
-                value={ symbol }
-                onChange={e => setSymbol(e.target.value)}
+        <div class="modal-dialog modal-dialog-centered" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 style={{color: 'green'}} class="modal-title" id="exampleModalLabel">Edit To-Do</h5>
+              <button
+                type="button"
+                class="btn-close"
+                aria-label="Close"
+                onClick={handleClose}
               >
-              </input>
-            </div> 
-             <div class="form-group row">
-              <label for="Name" class="col-sm-2 col-form-label" >Name</label>
-              <input
-                type="text"
-                class="form-control form-control-sm"
-                id="Name"
-                placeholder="Name"
-                value={ stock_name }
-                onChange={e => setStockName(e.target.value)}
-              >
-              </input>
+              </button>
             </div>
-            <div class="form-group row">
-              <label for="Price" class="col-sm-2 col-form-label" >Price</label>
-              <input
-                type="text"
-                class="form-control form-control-sm"
-                id="Price"
-                placeholder="Price"
-                value={ price }
-                onChange={e => setPrice(e.target.value)}
-              >
-              </input>
-            </div>   
-           </form>
-          </div>
+            <div class="modal-body">
+              <form>
+                <div class="form-group row">
+                  <label for="Symbol" class="col-sm-2 col-form-label" >Symbol</label>
+                  <input 
+                    type="text"
+                    class="form-control form-control-sm"
+                    id="Symbol"
+                    placeholder="Symbol"
+                    value={ symbol }
+                    onChange={e => setSymbol(e.target.value)}
+                  >
+                  </input>
+                </div> 
+                <div class="form-group row">
+                  <label for="Name" class="col-sm-2 col-form-label" >Name</label>
+                  <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    id="Name"
+                    placeholder="Name"
+                    value={ stock_name }
+                    onChange={e => setStockName(e.target.value)}
+                  >
+                  </input>
+                </div>
+                <div class="form-group row">
+                  <label for="Price" class="col-sm-2 col-form-label" >Price</label>
+                  <input
+                    type="text"
+                    class="form-control form-control-sm"
+                    id="Price"
+                    placeholder="Price"
+                    value={ price }
+                    onChange={e => setPrice(e.target.value)}
+                  >
+                  </input>
+                </div>   
+              </form>
+            </div>
             <div class="modal-footer">
-                <button
-                    type="button"
-                    class="btn btn-outline-success btn-sm"
-                    data-dismiss="modal"
-                    onClick={e => updateStock(e)}
-                >
-                        Edit
-                </button>
+              <button
+                  type="button"
+                  class="btn btn-outline-success btn-sm"
+                  data-dismiss="modal"
+                  onClick={e => updateStock(e)}
+              >
+                      Edit
+              </button>
             </div>
           </div>
+        </div>
       </div>
-  </div>
-
-
-
-
 
     </div>
   )
